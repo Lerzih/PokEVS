bfce24f9512be90338ff2ca54eb4bb5edbe53829
Lerzih <lerzihbarriales@gmail.com>
2023-08-23 09:53:48 +0100

    [:building_construction:] Add PokemonRepository in Infraestructure layer

---
+++ src/infrastructure/repositories/PokemonRepository.ts
@@ -0,0 +1,4 @@
+import { Pokemon } from "../../domain/models/Pokemon";
+import { PokemonRepository } from "../../domain/repositories/PokemonRepository";
+import { Http } from "../../domain/repositories/Http";
+
