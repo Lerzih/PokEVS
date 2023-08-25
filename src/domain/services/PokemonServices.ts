e7ff9e91e369e1e7c7cfc76d0d7812ec300f09d3
Lerzih <lerzihbarriales@gmail.com>
2023-08-25 11:51:35 +0100

    [:necktie:] Modify Interface to accept null values

--- src/domain/services/PokemonServices.ts
+++ src/domain/services/PokemonServices.ts
@@ -1,6 +1,6 @@
 import { Pokemon } from '../models/Pokemon';
 
 export interface PokemonService {
-    getPokemon: (id: number) => Promise<Pokemon>,
-    getAllPokemon: () => Promise<Pokemon[]>
+    getPokemon: (id: number) => Promise<Pokemon | null>,
+    getAllPokemon: () => Promise<Pokemon[] | null>
 }
